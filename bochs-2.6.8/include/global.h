182f84401ee8ef96858a8573c2ba358ddef8a68e
KellyLC <772812969@qq.com>
2017-07-18 07:55:42 -0700

    finishes 9

--- bochs-2.6.8/include/global.h
+++ bochs-2.6.8/include/global.h
@@ -11,8 +11,31 @@
 #define	EXTERN
 #endif
 
-EXTERN	int		disp_pos;
-EXTERN	u8		gdt_ptr[6];	/* 0~15:Limit  16~47:Base */
-EXTERN	DESCRIPTOR	gdt[GDT_SIZE];
-EXTERN	u8		idt_ptr[6];	/* 0~15:Limit  16~47:Base */
-EXTERN	GATE		idt[IDT_SIZE];
+EXTERN	int	ticks;
+
+EXTERN	int	disp_pos;
+
+EXTERN	u8			gdt_ptr[6];	/* 0~15:Limit  16~47:Base */
+EXTERN	struct descriptor	gdt[GDT_SIZE];
+EXTERN	u8			idt_ptr[6];	/* 0~15:Limit  16~47:Base */
+EXTERN	struct gate		idt[IDT_SIZE];
+
+EXTERN	u32	k_reenter;
+EXTERN	int	nr_current_console;
+
+EXTERN	struct tss	tss;
+EXTERN	struct proc*	p_proc_ready;
+
+extern	char		task_stack[];
+extern	struct proc	proc_table[];
+extern  struct task	task_table[];
+extern  struct task	user_proc_table[];
+extern	irq_handler	irq_table[];
+extern	TTY		tty_table[];
+extern  CONSOLE		console_table[];
+
+/* FS */
+extern	u8 *			fsbuf;
+extern	const int		FSBUF_SIZE;
+extern	struct dev_drv_map	dd_map[];
+
--- bochs-2.6.8/kernel/protect.c
+++ bochs-2.6.8/kernel/protect.c
@@ -6,14 +6,22 @@
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
 
 #include "type.h"
+#include "stdio.h"
 #include "const.h"
 #include "protect.h"
+#include "fs.h"
+#include "tty.h"
+#include "console.h"
+#include "proc.h"
+#include "string.h"
 #include "global.h"
 #include "proto.h"
 
+
 /* 本文件内函数声明 */
-PRIVATE void init_idt_desc(unsigned char vector, u8 desc_type,
-			   int_handler handler, unsigned char privilege);
+PRIVATE void init_idt_desc(unsigned char vector, u8 desc_type, int_handler handler, unsigned char privilege);
+PRIVATE void init_descriptor(struct descriptor * p_desc, u32 base, u32 limit, u16 attribute);
+
 
 /* 中断处理函数 */
 void	divide_error();
@@ -32,31 +40,34 @@ void	stack_exception();
 void	general_protection();
 void	page_fault();
 void	copr_error();
-void    hwint00();
-void    hwint01();
-void    hwint02();
-void    hwint03();
-void    hwint04();
-void    hwint05();
-void    hwint06();
-void    hwint07();
-void    hwint08();
-void    hwint09();
-void    hwint10();
-void    hwint11();
-void    hwint12();
-void    hwint13();
-void    hwint14();
-void    hwint15();
+void	hwint00();
+void	hwint01();
+void	hwint02();
+void	hwint03();
+void	hwint04();
+void	hwint05();
+void	hwint06();
+void	hwint07();
+void	hwint08();
+void	hwint09();
+void	hwint10();
+void	hwint11();
+void	hwint12();
+void	hwint13();
+void	hwint14();
+void	hwint15();
+
 
 /*======================================================================*
                             init_prot
+ *----------------------------------------------------------------------*
+ 初始化 IDT
  *======================================================================*/
 PUBLIC void init_prot()
 {
 	init_8259A();
 
-	// 全部初始化成中断门(没有陷阱门)
+	/* 全部初始化成中断门(没有陷阱门) */
 	init_idt_desc(INT_VECTOR_DIVIDE,	DA_386IGate,
 		      divide_error,		PRIVILEGE_KRNL);
 
@@ -152,18 +163,44 @@ PUBLIC void init_prot()
 
         init_idt_desc(INT_VECTOR_IRQ8 + 7,      DA_386IGate,
                       hwint15,                  PRIVILEGE_KRNL);
+
+	init_idt_desc(INT_VECTOR_SYS_CALL,	DA_386IGate,
+		      sys_call,			PRIVILEGE_USER);
+
+	/* 填充 GDT 中 tss 这个描述符 */
+	memset(&tss, 0, sizeof(tss));
+	tss.ss0		= SELECTOR_KERNEL_DS;
+	init_descriptor(&gdt[INDEX_TSS],
+			vir2phys(seg2phys(SELECTOR_KERNEL_DS), &tss),
+			sizeof(tss) - 1,
+			DA_386TSS);
+	tss.iobase	= sizeof(tss);	/* 没有I/O许可位图 */
+
+	/* 填充 GDT 中进程的 LDT 的描述符 */
+	int i;
+	struct proc* p_proc = proc_table;
+	u16 selector_ldt = INDEX_LDT_FIRST << 3;
+	for (i = 0; i < NR_TASKS+NR_PROCS; i++){
+		init_descriptor(&gdt[selector_ldt>>3],
+				vir2phys(seg2phys(SELECTOR_KERNEL_DS),
+					proc_table[i].ldts),
+				LDT_SIZE * sizeof(struct descriptor) - 1,
+				DA_LDT);
+		p_proc++;
+		selector_ldt += 1 << 3;
+	}
 }
+
 
 /*======================================================================*
                              init_idt_desc
  *----------------------------------------------------------------------*
  初始化 386 中断门
  *======================================================================*/
-PRIVATE void init_idt_desc(unsigned char vector, u8 desc_type,
-			  int_handler handler, unsigned char privilege)
+PUBLIC void init_idt_desc(unsigned char vector, u8 desc_type, int_handler handler, unsigned char privilege)
 {
-	GATE *	p_gate	= &idt[vector];
-	u32	base	= (u32)handler;
+	struct gate * p_gate	= &idt[vector];
+	u32 base = (u32)handler;
 	p_gate->offset_low	= base & 0xFFFF;
 	p_gate->selector	= SELECTOR_KERNEL_CS;
 	p_gate->dcount		= 0;
@@ -171,37 +208,65 @@ PRIVATE void init_idt_desc(unsigned char vector, u8 desc_type,
 	p_gate->offset_high	= (base >> 16) & 0xFFFF;
 }
 
+
+/*======================================================================*
+                           seg2phys
+ *----------------------------------------------------------------------*
+ 由段名求绝对地址
+ *======================================================================*/
+PUBLIC u32 seg2phys(u16 seg)
+{
+	struct descriptor* p_dest = &gdt[seg >> 3];
+
+	return (p_dest->base_high << 24) | (p_dest->base_mid << 16) | (p_dest->base_low);
+}
+
+/*======================================================================*
+                           init_descriptor
+ *----------------------------------------------------------------------*
+ 初始化段描述符
+ *======================================================================*/
+PRIVATE void init_descriptor(struct descriptor * p_desc, u32 base, u32 limit, u16 attribute)
+{
+	p_desc->limit_low	= limit & 0x0FFFF;		/* 段界限 1		(2 字节) */
+	p_desc->base_low	= base & 0x0FFFF;		/* 段基址 1		(2 字节) */
+	p_desc->base_mid	= (base >> 16) & 0x0FF;		/* 段基址 2		(1 字节) */
+	p_desc->attr1		= attribute & 0xFF;		/* 属性 1 */
+	p_desc->limit_high_attr2= ((limit >> 16) & 0x0F) |
+				  ((attribute >> 8) & 0xF0);	/* 段界限 2 + 属性 2 */
+	p_desc->base_high	= (base >> 24) & 0x0FF;		/* 段基址 3		(1 字节) */
+}
+
 /*======================================================================*
                             exception_handler
  *----------------------------------------------------------------------*
  异常处理
  *======================================================================*/
-PUBLIC void exception_handler(int vec_no,int err_code,int eip,int cs,int eflags)
+PUBLIC void exception_handler(int vec_no, int err_code, int eip, int cs, int eflags)
 {
 	int i;
 	int text_color = 0x74; /* 灰底红字 */
-
-	char * err_msg[] = {"#DE Divide Error",
-			    "#DB RESERVED",
-			    "—  NMI Interrupt",
-			    "#BP Breakpoint",
-			    "#OF Overflow",
-			    "#BR BOUND Range Exceeded",
-			    "#UD Invalid Opcode (Undefined Opcode)",
-			    "#NM Device Not Available (No Math Coprocessor)",
-			    "#DF Double Fault",
-			    "    Coprocessor Segment Overrun (reserved)",
-			    "#TS Invalid TSS",
-			    "#NP Segment Not Present",
-			    "#SS Stack-Segment Fault",
-			    "#GP General Protection",
-			    "#PF Page Fault",
-			    "—  (Intel reserved. Do not use.)",
-			    "#MF x87 FPU Floating-Point Error (Math Fault)",
-			    "#AC Alignment Check",
-			    "#MC Machine Check",
-			    "#XF SIMD Floating-Point Exception"
-	};
+	char err_description[][64] = {	"#DE Divide Error",
+					"#DB RESERVED",
+					"—  NMI Interrupt",
+					"#BP Breakpoint",
+					"#OF Overflow",
+					"#BR BOUND Range Exceeded",
+					"#UD Invalid Opcode (Undefined Opcode)",
+					"#NM Device Not Available (No Math Coprocessor)",
+					"#DF Double Fault",
+					"    Coprocessor Segment Overrun (reserved)",
+					"#TS Invalid TSS",
+					"#NP Segment Not Present",
+					"#SS Stack-Segment Fault",
+					"#GP General Protection",
+					"#PF Page Fault",
+					"—  (Intel reserved. Do not use.)",
+					"#MF x87 FPU Floating-Point Error (Math Fault)",
+					"#AC Alignment Check",
+					"#MC Machine Check",
+					"#XF SIMD Floating-Point Exception"
+				};
 
 	/* 通过打印空格的方式清空屏幕的前五行，并把 disp_pos 清零 */
 	disp_pos = 0;
@@ -211,7 +276,7 @@ PUBLIC void exception_handler(int vec_no,int err_code,int eip,int cs,int eflags)
 	disp_pos = 0;
 
 	disp_color_str("Exception! --> ", text_color);
-	disp_color_str(err_msg[vec_no], text_color);
+	disp_color_str(err_description[vec_no], text_color);
 	disp_color_str("\n\n", text_color);
 	disp_color_str("EFLAGS:", text_color);
 	disp_int(eflags);
